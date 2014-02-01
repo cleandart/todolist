a87e98d763ed2f837a5757783c2f8d449d3f69ea
mama <tomas.kulich@gmail.com>
2014-02-01 15:21:07 +0100

    dnd

--- lib/item.dart
+++ lib/item.dart
@@ -1,11 +1,13 @@
 library item;
 
 import 'package:react/react.dart';
+import 'package:clean_data/clean_data.dart';
 
 var itemComponent = registerComponent(() => new ItemComponent());
 
 class ItemComponent extends Component {
-  get item => props['item'];
+  DataSet get items => props['items'];
+  DataMap get item => props['item'];
   get text => item.ref('text');
   get done => item.ref('done');
   var _listener;
@@ -19,20 +21,38 @@ class ItemComponent extends Component {
   }
 
   render() {
-    return div({}, [input({'type': 'checkbox', 'checked': done.value, 'onChange': onBoxChange}),
+    return div({'draggable': 'true', 'onDragStart': drag, 'onDrop': drop, 'onDragOver': allowDrop },
+                    [input({'type': 'checkbox', 'checked': done.value, 'onChange': onBoxChange}),
+                    span({'className': 'dnd'}, 'DND'),
                     input({'value': text.value, 'onChange': onTextChange})]);
   }
 
   onTextChange (e) {
     text.value = e.target.value;
-    print(item);
     redraw();
+  }
 
+  allowDrop(ev) {
+    ev.nativeEvent.preventDefault();
+  }
+
+  drop(ev){
+    ev.nativeEvent.preventDefault();
+    print('drop: ${ev.nativeEvent}');
+    var id = ev.nativeEvent.dataTransfer.getData("id");
+    DataMap other = items.findBy('_id', id).first;
+    other['order'] = item['order']-0.1;
+    print('item: $item');
+    print('other: $other');
+  }
+
+  drag(ev){
+    ev.nativeEvent.dataTransfer.setData("id",item['_id']);
+    print('drag: ${ev.nativeEvent}');
   }
 
   onBoxChange(e) {
     done.value = !done.value;
-    print(item);
     redraw();
   }
 }
@@ -40,17 +60,29 @@ class ItemComponent extends Component {
 var itemList = registerComponent(() => new ItemList());
 
 class ItemList extends Component {
-  get items => props['items'];
+  DataSet get items => props['items'];
+
+  var _listener;
+
 
   List _renderItems() {
-    return items.map((item) => itemComponent({'item': item})).toList();
+    num order = 0;
+    for (DataMap d in items){
+      if (!d.containsKey('order')){
+        print('adding order');
+        d['order'] = order++;
+      }
+    }
+    var sorted = items.liveSortByKey((d) => d['order']);
+    return sorted.map((item) => itemComponent({'item': item, 'items': items})).toList();
   }
+
   componentWillMount() {
     items.onChange.listen((_) => redraw());
   }
 
   render() {
-    print(items);
+    print('rendering intems');
     return div({}, _renderItems());
   }
 
